e1d6792bacaad86411234cecd71a560615088c1d
akihiro.matsuyama/akidon <akihiro.matsuyama@sansan.com>
2025-09-25 23:19:40 +0900

    xcode作成

---
+++ localDB-sampleer/localDB-sampleer/localDB_sampleerApp.swift
@@ -0,0 +1,17 @@
+//
+//  localDB_sampleerApp.swift
+//  localDB-sampleer
+//
+//  Created by Akihiro Matsuyama on 2025/09/25.
+//
+
+import SwiftUI
+
+@main
+struct localDB_sampleerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
